6e6d837cca0e1b945e4fca65ea6a4594ddfabe23
krasilnikovd <vovchukdurak@gmail.com>
2022-12-05 20:24:20 +0200

    Додано підрахунок суми чисел і виведення результату

--- mtzkit/mtzkit/Program.cs
+++ mtzkit/mtzkit/Program.cs
@@ -10,7 +10,9 @@ namespace mtzkit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5+10={}",x+y);
             Console.ReadKey();
         }
     }
